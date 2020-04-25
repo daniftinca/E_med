df7584326521edf68e3e9e16b51272f062fd9e99
Dan <daniftincaalexandru@gmail.com>
2020-04-25 23:01:25 +0300

    ntohign

--- E_med/settings.py
+++ E_med/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'e_med',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'Postgres1234',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
