af0b5c02428198609eff5998a1788c425b42bc29
Dan <daniftincaalexandru@gmail.com>
2020-04-26 03:13:13 +0300

    asdfasdfasfa

--- E_med/settings.py
+++ E_med/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'bad_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('SECRET_KEY', False)
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
